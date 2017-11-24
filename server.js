2e6d96d88cde1e88ee40ec6f05ef6691f9f543bb
neliza <nicolasaelf64@gmail.com>
2017-11-24 09:40:24 -0700

    still a big problem

--- server.js
+++ server.js
@@ -9,14 +9,11 @@ const request = require("request");
 const cheerio = require("cheerio");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
+const axios = require("axios");
 
 // Initialize Express
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-//Require in routes
-const routes = require("./routing/routes.js")
 
 // Database configuration
 const databaseUrl = "scraper";
@@ -36,9 +33,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 // Use express.static to serve the public folder as a static directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes)
+app.use(express.static("public"));
 
 // Require in models
 const db = require('./models');
@@ -46,9 +41,10 @@ const db = require('./models');
 app.engine('handlebars', exphbs({ defaultLayout: "main" }));
 app.set('view engine', 'handlebars');
 
+//Require in routes
+require("./routing/routes.js")(app);
+
 // Listen on port 3000
 app.listen(3000, function() {
     console.log("App running on port 3000!");
   });
-
-// module.exports = app;
